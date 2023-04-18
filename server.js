0b8ac7b392a1a023a2ddb67fe3715f7086376e21
Theau Benezet <theau.benezet@epitech.eu>
2023-04-18 12:31:25 +0100

    Tu peux delete bg

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 const app = require('express');
+const router = app.Router();
 const uri = 'mongodb+srv://monkeymartin86:123soleil@wtasscluster.uq13ht3.mongodb.net/Ass4';
 
+app.use(express.json());
+
+app.listen(3000, () => console.log('Server started'));
+
+
+//Function to connect to MongoDB
 async function connect() {
     try {
         await mongoose.connect(uri);
@@ -10,5 +17,7 @@ async function connect() {
         console.log('Connect failure');
     }
 }
+
+
 
 connect();
