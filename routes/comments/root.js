855e5a9041ddf1a24e24427d9e2269ee5dee432e
fowen <stephane1.ganivet@epitech.eu>
2023-05-02 13:47:37 +0100

    [+] add updateComment route

--- routes/comments/root.js
+++ routes/comments/root.js
@@ -1,8 +1,9 @@
-const { createComment, getComments, getComment, deleteComment } = require('../../controllers/comment');
+const { createComment, getComments, getComment, deleteComment, updateComment } = require('../../controllers/comment');
 
 module.exports = async function (fastify) {
     fastify.post('/', createComment);
     fastify.get('/', getComments);
     fastify.get('/:id', getComment);
     fastify.delete('/:id', deleteComment);
+    fastify.put('/:id', updateComment);
 }
